40df5c355a29247241b6f133fc2bed3bd0a6986c
Anton Lagutin <alagutin@mtt.ru>
2022-10-30 22:15:12 +0300

    fix task_2

--- task_2/conftest.py
+++ task_2/conftest.py
@@ -3,7 +3,6 @@ import logging
 import pytest
 from selenium import webdriver
 from selene.support.shared import config
-from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from helpers.base_helper import get_css_files
@@ -17,21 +16,12 @@ def pytest_addoption(parser):
                      choices=['chrome'],
                      default='chrome')
 
-    parser.addoption('--headless',
-                     help=u'Headless mode',
-                     choices=['y', 'n'],
-                     default='n')
 
-
-def custom_driver(t_browser, t_headless):
+def custom_driver(t_browser):
     """ Custom driver """
     logging.debug('custom driver config start')
     if t_browser == 'chrome':
-        if t_headless == 'y':
-            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                                      options=headless_chrome_options())
-        else:
-            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     else:
         raise ValueError('t_browser does not set')
     driver.maximize_window()
@@ -41,24 +31,6 @@ def custom_driver(t_browser, t_headless):
     return driver
 
 
-def headless_chrome_options():
-    """ Custom chrome options """
-    logging.info('set chromedriver options start')
-    chrome_options = Options()
-    chrome_options.set_capability("pageLoadStrategy", "eager")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-notifications")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-infobars")
-    chrome_options.add_argument("--enable-automation")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-setuid-sandbox")
-    logging.info('set chromedriver options finish')
-    return chrome_options
-
-
 @pytest.fixture(scope='session')
 def t_browser(request):
     """  Test browser. Params: [chrome, opera, firefox].  """
@@ -66,23 +38,23 @@ def t_browser(request):
 
 
 @pytest.fixture(scope='session')
-def t_headless(request):
-    return request.config.getoption('--headless')
-
-
-@pytest.fixture(scope='session')
-def browser_session(t_browser, t_headless):
-    config.driver = custom_driver(t_browser, t_headless)
+def browser_session(t_browser):
+    config.driver = custom_driver(t_browser)
     yield config.driver
     config.driver.quit()
 
 
 @pytest.fixture()
 def rename_file(request, iterate):
-    css_files = get_css_files()
-    path = os.getcwd()
-    os.rename(f'{path}{PATH_FILES_SITE}/{css_files[iterate]}', f'{path}{PATH_FILES_SITE}/new{css_files[iterate]}')
+    def _rename_file(css_files):
+        path = os.getcwd()
+        os.rename(f'{path}{PATH_FILES_SITE}/{css_files[iterate]}',
+                  f'{path}{PATH_FILES_SITE}/new{css_files[iterate]}')
 
-    def return_file_name():
-        os.rename(f'{path}{PATH_FILES_SITE}/new{css_files[iterate]}', f'{path}{PATH_FILES_SITE}/{css_files[iterate]}')
-    request.addfinalizer(return_file_name)
+        def _return_file_name():
+            os.rename(f'{path}{PATH_FILES_SITE}/new{css_files[iterate]}',
+                      f'{path}{PATH_FILES_SITE}/{css_files[iterate]}')
+
+        request.addfinalizer(_return_file_name)
+
+    return _rename_file
--- task_2/tests/test_base_page.py
+++ task_2/tests/test_base_page.py
@@ -1,12 +1,15 @@
 import pytest
 from helpers.base_helper import get_css_files
 from page.base_page import BasePage
+import os
+from settings import PATH_FILES_SITE
 
 css_files = get_css_files()
 
 
 @pytest.mark.parametrize('iterate', range(len(css_files)))
 def test_check_h2_headers_of_homepage(browser_session, rename_file, iterate):
+    rename_file(css_files)
     base_page = BasePage()
     base_page.open_site()
     base_page.check_color_headings(
